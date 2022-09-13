ab43d794d7956e202fb3fe5e899986dc588181e5
pranav s <dev.pranabsk@gmail.com>
2022-09-13 11:10:39 +0530

    Book component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Book from './components/Book/Book';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,6 +14,7 @@ function App() {
     <Header/>
     <Switch>
       <Route path="/home"><Home /></Route>
+      <Route path="/book/:bedType"><Book /></Route>
       <Route path="/"><Home /></Route>
     </Switch>
 </Router>
